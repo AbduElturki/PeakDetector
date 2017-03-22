774e8bb790d3ff2f8473947695ea9b0b87026247
Vlad Ellis <me@ellisvlad.com>
2017-03-22 11:53:03 +0000

    Some changes n stuff :D

--- CmdProc.vhd
+++ CmdProc.vhd
@@ -1,58 +1,64 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-use ieee.numeric_std_unsigned.all;
 use work.common_pack.all;
 library UNISIM;
 use UNISIM.VCOMPONENTS.ALL;
-use UNISIM.VPKG.ALL;
 
 entity cmdProc is
   port (
-      clk:		in std_logic;
-      reset:		in std_logic;
-      rxnow:		in std_logic;
-      rxData:			in std_logic_vector (7 downto 0);
-      txData:			out std_logic_vector (7 downto 0);
-      rxdone:		out std_logic;
-      ovErr:		in std_logic;
-      framErr:	in std_logic;
-      txnow:		out std_logic;
-      txdone:		in std_logic;
-      start: out std_logic;
-      numWords_bcd: out BCD_ARRAY_TYPE(2 downto 0);
-      dataReady: in std_logic;
-      byte: in std_logic_vector(7 downto 0);
-      maxIndex: in BCD_ARRAY_TYPE(2 downto 0);
-      dataResults: in CHAR_ARRAY_TYPE(0 to RESULT_BYTE_NUM-1);
-      seqDone: in std_logic
-    );
-end;
+    clk: in std_logic;
+    reset: in std_logic;
+    rxnow: in std_logic;
+    rxData: in std_logic_vector (7 downto 0);
+    txData: out std_logic_vector (7 downto 0);
+    rxdone: out std_logic;
+    ovErr: in std_logic;
+    framErr: in std_logic;
+    txnow: out std_logic;
+    txdone: in std_logic;
+    start: out std_logic;
+    numWords_bcd: out BCD_ARRAY_TYPE(2 downto 0);
+    dataReady: in std_logic;
+    byte: in std_logic_vector(7 downto 0);
+    maxIndex: in BCD_ARRAY_TYPE(2 downto 0);
+    dataResults: in CHAR_ARRAY_TYPE(0 to RESULT_BYTE_NUM-1);
+    seqDone: in std_logic
+  );
+end entity;
 
 architecture commands of cmdProc is
-  type state_type is (CommandLetter, Cmd0, Cmd1, Cmd2, StartCountingBitches, Waitfordata, Sendfirstbyte, SendSecondbyte, SendSpacebyte);
+  type state_type is (CommandLetter, Number0, Cmd1, Cmd2, StartCountingBitches, Waitfordata, Sendfirstbyte, SendSecondbyte, SendSpacebyte);
   signal curState, nextState: state_type;
   signal n0, n1, n2: std_logic_vector(7 downto 0);
 begin
-  combi_nextState: process(curState, rxdata)
+  combi_nextState: process(curState, rxNow)
   begin
-    
     start <= '0';
+	 txNow <= '0';
     
-    case curState is
+    
+	if rxnow='1' then
+	case curState is
     when CommandLetter =>
-      if rxdata="01000001" or rxdata="01100001" then -- if data = a or A
-        nextState <= Cmd0;
-    else
-      nextState <= CommandLetter;
-    end if;
+      if rxdata="01000001" or rxdata="01100001" then -- a or A
+        nextState <= Number0;
+		  txData <= "01010010";
+		  txNow <= '1';
+		else
+			nextState <= CommandLetter;
+		end if;
     
-    when Cmd0 =>
-      if rxdata(3 downto 0) > "1001" or rxdata(7 downto 4) /= "0011" then
-        nextState <= CommandLetter;
-      else
+    when Number0 => --00110000 48 |   00111001 57
+      if rxdata>="00110000" and rxdata<="00111001" then --Number
+        nextState <= Number0;
         n0 <= rxdata;
-        nextState <= Cmd1;
+		  txData <= "01010000";
+		  txNow <= '1';
+      else
+        nextState <= CommandLetter;
+		  txData <= "01010001";
+		  txNow <= '1';
       end if;
       
     when Cmd1 =>
@@ -74,9 +80,9 @@ begin
       
       when StartCountingBitches =>
         nextState <= Waitfordata;
-        numWords_bcd(0)<=n0(3 downto 0);
-        numWords_bcd(1)<=n1(3 downto 0);
-        numWords_bcd(2)<=n2(3 downto 0);
+        --numWords_bcd(0)<=n0(3 downto 0);
+        --numWords_bcd(1)<=n1(3 downto 0);
+        --snumWords_bcd(2)<=n2(3 downto 0);
         start <= '1';
       
       
@@ -89,7 +95,7 @@ begin
         
       when Sendfirstbyte => 
         if (byte(7 downto 4)>"1001") then
-          txData<="0100" & byte(7 downto 4)-"1001"; -- A-F
+          txData<="0100" & std_logic_vector(unsigned(byte(7 downto 4))-"1001"); -- A-F
         else
           txData<="0011" & byte(7 downto 4); -- 0-9
         end if;
@@ -98,7 +104,7 @@ begin
         
       when Sendsecondbyte =>
         if (byte(3 downto 0)>"1001") then
-          txData<="0100" & byte(3 downto 0)-"1001"; -- A-F
+          txData<="0100" & std_logic_vector(unsigned(byte(3 downto 0))-"1001"); -- A-F
         else
           txData<="0011" & byte(3 downto 0); -- 0-9
         end if;
@@ -114,10 +120,9 @@ begin
           start <= '1';
           nextState <= WaitForData;
         end if;
-        
   end case;
+  end if;
 end process;
-
 
 seq_state: process (clk, reset)
   begin
